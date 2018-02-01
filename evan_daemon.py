fa1ba7cb1208d39e81b4d7c9efb81cc34c22bc2b
evan lou <evan_lou@126.com>
2018-02-01 15:30:47 +0800

    fixed save config.json bug
    Signed-off-by: evan lou <evan_lou@126.com>

--- evan_daemon.py
+++ evan_daemon.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import easyquotation
 
 import easyquant
--- strategies/my_strategies.py
+++ strategies/my_strategies.py
@@ -18,21 +18,22 @@ class Strategy(StrategyTemplate):
                 self.config[key] = str(high * 0.91)
                 self.dict2file('config.json', self.config)
 
+            baifenbi = (1 - zhisun / now) * 100.0
             if now > zhisun:
-                baifenbi = (1 - zhisun / now) * 100.0
                 self.log.info('%s now:%.2f' % (data['name'], now))
                 self.log.info('止损价:%.2f 止损百分比:%.2f%% high:%.2f\n' % (zhisun, baifenbi, high))
             else:
                 self.log.info('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
-                self.log.info('!!!%s now:%.2f 止损价:%.2f high:%.2f!!!' % (data['name'], now, zhisun, high))
-                self.log.info('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+                self.log.info('%s now:%.2f' % (data['name'], now))
+                self.log.info('止损价:%.2f 止损百分比:%.2f%% high:%.2f' % (zhisun, baifenbi, high))
+                self.log.info('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n')
 
     def file2dict(self, path):
-        with open(path, mode='r+', encoding='utf-8') as f:
+        with open(path, mode='r', encoding='utf-8') as f:
             return json.load(f) 
 
     def dict2file(self, path, data):
-        with open(path, mode='r+', encoding='utf-8') as f:
+        with open(path, mode='w', encoding='utf-8') as f:
             json_str = json.dumps(data)
             f.write(json_str)
             f.flush()
